7572a28eade88feb52c64e4239bd67ebaa9e8cb8
Ricardo Vaz Mannrich <mannrich@hotmail.com>
2026-04-24 21:50:54 -0300

    feat: add custom styles and integrate Aura theme stylesheet in Front

--- besttravel-front-bff/src/main/java/dev/ricardovm/besttravel/front/ui/AppConfig.java
+++ besttravel-front-bff/src/main/java/dev/ricardovm/besttravel/front/ui/AppConfig.java
@@ -1,9 +1,13 @@
 package dev.ricardovm.besttravel.front.ui;
 
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.shared.ui.Transport;
+import com.vaadin.flow.theme.aura.Aura;
 
+@StyleSheet(Aura.STYLESHEET)
+@StyleSheet("styles.css")
 @Push(transport = Transport.LONG_POLLING)
 public class AppConfig implements AppShellConfigurator {
 }
